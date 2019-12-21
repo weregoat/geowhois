e7a10811db057f87b506101c4a46fa7f408b5939
Ruggero Cucchiani <weregoat.github@maialinux.org>
2019-12-21 10:03:49 +0100

    made client public

--- pkg/whois/client.go
+++ pkg/whois/client.go
@@ -5,17 +5,17 @@ import (
 	"github.com/weregoat/goat-whois/pkg/whois/sources"
 )
 
-type client struct {
+type Client struct {
 	Sources []sources.Source
 }
 
-func New(sources ...sources.Source) client {
-	c := client{}
+func New(sources ...sources.Source) Client {
+	c := Client{}
 	c.Sources = sources
 	return c
 }
 
-func (c *client) Query(resource string) (data response.Response) {
+func (c Client) Query(resource string) (data response.Response) {
 	for _, source := range c.Sources {
 		data = source.Query(resource)
 		if data.IsValid() {
